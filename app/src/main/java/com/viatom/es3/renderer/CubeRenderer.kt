c6f441ba723e217d7be6c100cb322cfa46aa7c1c
李光浩 <531147618@qq.com>
2021-03-10 16:29:36 +0800

    renderer4

--- app/src/main/java/com/viatom/es3/renderer/CubeRenderer.kt
+++ app/src/main/java/com/viatom/es3/renderer/CubeRenderer.kt
@@ -25,40 +25,26 @@ class CubeRenderer : GLSurfaceView.Renderer {
     /**
      * 点的坐标
      */
-    private val vertexPoints = floatArrayOf( //正面矩形
-        0.25f, 0.25f, 0.0f,  //V0
-        -0.75f, 0.25f, 0.0f,  //V1
-        -0.75f, -0.75f, 0.0f,  //V2
-        0.25f, -0.75f, 0.0f,  //V3
-        //背面矩形
-        0.75f, -0.25f, 0.0f,  //V4
-        0.75f, 0.75f, 0.0f,  //V5
-        -0.25f, 0.75f, 0.0f,  //V6
-        -0.25f, -0.25f, 0.0f //V7
+    private val vertexPoints = floatArrayOf(
+        0.5f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+        -0.5f, -0.0f, 0.0f,
+
     )
 
     /**
      * 定义索引
      */
-    private val indices = shortArrayOf( //背面
-        5, 6, 7, 5, 7, 4,  //左侧
-        6, 1, 2, 6, 2, 7,  //底部
-        4, 7, 2, 4, 2, 3,  //顶面
-        5, 6, 7, 5, 7, 4,  //右侧
-        5, 0, 3, 5, 3, 4,  //正面
-        0, 1, 2, 0, 2, 3
+    private val indices = shortArrayOf(
+       0,1,2
     )
 
     //立方体的顶点颜色
     private val colors = floatArrayOf(
-        0.3f, 0.4f, 0.5f, 1f,  //V0
-        0.3f, 0.4f, 0.5f, 1f,  //V1
-        0.3f, 0.4f, 0.5f, 1f,  //V2
-        0.3f, 0.4f, 0.5f, 1f,  //V3
-        0.6f, 0.5f, 0.4f, 1f,  //V4
-        0.6f, 0.5f, 0.4f, 1f,  //V5
-        0.6f, 0.5f, 0.4f, 1f,  //V6
-        0.6f, 0.5f, 0.4f, 1f //V7
+       1f, 0.0f, 0.0f, 1f,  //V0
+        0f, 1f, 0.0f, 1f,  //V1
+        0f, 0.0f, 1f, 1f,  //V2
+
     )
 
     override fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
